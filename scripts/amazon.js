012edd76e73d4848e7327535898c21563e6f6d4a
Ajay <ajay@gmail.com>
2025-08-17 22:33:30 +0530

    For product i use different js file that is products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,45 +1,7 @@
-const product = [
-{
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: " Black and Gray Athletic Cotton Socks - 6 Pairs",
-
-    rating:{
-        rate: 4.5,
-        counts: 87
-    },
-
-    priceCents: 1090
-},
-
-{
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name : " Intermediate Size Basketball",
-
-    rating:{
-        rate: 4,
-        counts: 127
-    },
-
-    priceCents:2095
-},
-
-{
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name:  'Adults Plain Cotton T-Shirt - 2 Pack',
-
-    rating:{
-        rate: 4,
-        counts: 127
-    },
-
-    priceCents:799
-
-}
-];
 
 let productHTML = "";
 
-product.forEach((value,index) => {
+products.forEach((value,index) => {
     productHTML += `
     <div class="product-container">
         <div class="product-image-container">
@@ -53,9 +15,9 @@ product.forEach((value,index) => {
 
         <div class="product-rating-container">
             <img class="product-rating-stars"
-            src="images/ratings/rating-${value.rating.rate * 10}.png">
+            src="images/ratings/rating-${value.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-            ${value.rating.counts}
+            ${value.rating.count}
             </div>
         </div>
 
