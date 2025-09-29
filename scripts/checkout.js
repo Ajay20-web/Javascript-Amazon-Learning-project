98b0a791ad9929eeab0ed280496a0937caa2e2b3
Ajay <ajay@gmail.com>
2025-09-29 23:14:23 +0530

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderPage } from "./checkout/ordersummery.js";
 import { renderPaymentSummery } from "./checkout/paymentsummery.js";
+import '../data/cart-oop.js';
 renderPage();
 renderPaymentSummery();
  
