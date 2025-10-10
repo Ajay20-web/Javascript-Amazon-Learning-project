7b8d9e9b2a101c7721ca10f889f7d3bb44ce7069
Ajay <ajay@gmail.com>
2025-10-10 19:51:27 +0530

    Add backend practice file for learning the Http request.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderPage } from "./checkout/ordersummery.js";
 import { renderPaymentSummery } from "./checkout/paymentsummery.js";
 import '../data/cart-oop.js';
 import '../data/cart-class.js';
+import '../data/backend-practice.js';
 renderPage();
 renderPaymentSummery();
  
