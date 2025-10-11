547ebe3e5c6f373810ac619e9958e285157247a7
Ajay <ajay@gmail.com>
2025-10-11 17:23:00 +0530

    Load products in beforeAll hook for testing

--- testing/test-jasmine/checkout/ordersummeryTest.js
+++ testing/test-jasmine/checkout/ordersummeryTest.js
@@ -1,10 +1,15 @@
 import { renderPage } from '../../../scripts/checkout/ordersummery.js';
 import { cart, loadingFormsStorage } from '../../../data/cart.js';
-
-console.log(cart);
+import { loadProduct , products } from '../../../data/products.js';
+console.log(products);
 
 // integration testing 
 describe('test suite: testing the renderPage function form orderSummery.js', ()=> {
+
+beforeAll((done)=>{
+  loadProduct(done, ()=>{}); //--> This loadProduct function is for getting the product data from the backend server and that done() function we give argument to the loadProduct() and we run the done() function after getting the data so that done() run the full test after getting the data from server.
+});
+
 
   let prodect1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   let prodect2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
@@ -51,4 +56,4 @@ describe('test suite: testing the renderPage function form orderSummery.js', ()=
   afterEach( ()=>{ //--> This just run after the it() block run this just clean up the html that's all.
     document.querySelector('.js-test-container').innerHTML ='';
   });
-});
+}); 
