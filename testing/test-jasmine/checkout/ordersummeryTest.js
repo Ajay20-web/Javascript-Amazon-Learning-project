8d773f76a8a2d5671e753eee6b31b0a661832ddc
Ajay <ajay@gmail.com>
2025-11-18 20:55:04 +0530

    Refactor ordersummeryTest.js to improve async handling and code readability for testing , Testing also done.

--- testing/test-jasmine/checkout/ordersummeryTest.js
+++ testing/test-jasmine/checkout/ordersummeryTest.js
@@ -1,45 +1,44 @@
 import { renderPage } from '../../../scripts/checkout/ordersummery.js';
-import { cart, loadingFormsStorage } from '../../../data/cart.js';
-import { products , loadFetch } from '../../../data/products.js';
-console.log(products);
+import { loadingFormsStorage } from '../../../data/cart.js';
+import { loadFetch } from '../../../data/products.js';
 
-// integration testing 
+// integration testing is the combination of unit testing and end to end testing
 describe('test suite: testing the renderPage function form orderSummery.js', ()=> {
 
-beforeAll((done)=>{
-  // loadProduct(done); //--> This loadProduct function is for getting the product data from the backend server and that done() function we give argument to the loadProduct() and we run the done() function after getting the data so that done() run the full test after getting the data from server.
- 
-  Promise.all([loadFetch()]).then(()=>{
-    done(); //-->This done() function run the full test after getting the data from server.
+  beforeAll((done)=>{
+   //loadProduct(done); //--> This loadProduct function is for getting the product data from the backend server and that done() function we give argument to the loadProduct() and we run the done() function after getting the data so that done() run the full test after getting the data from server.
+    async function fetchProducts(){
+        await loadFetch()
+        done(); //--> This done() function just tell the testing framework that the async operation is complete and now you can run the test cases.
+    }
+    fetchProducts();
   });
-
-});
-
 
   let prodect1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   let prodect2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+  
   beforeEach( () =>{ //-->This just run the beforeEach() function inside it() block first line that's the reason we put product1,product2 variables outside the beforeEach() function.
     spyOn(localStorage ,'setItem');
     document.querySelector('.js-test-container').innerHTML =`
-    <div class="order-summary"></div>
-    <div class="js-payment"></div>
+      <div class="order-summary"></div>
+      <div class="js-payment"></div>
     `;
-      spyOn(localStorage,'getItem').and.callFake( () =>{
-        return JSON.stringify([{
-          productid:prodect1,
-          quantity: 2,
-          deliveryOptionsid:'1'
-        },
-        {
-          productid:prodect2,
-          quantity: 1,
-          deliveryOptionsid:'2'
-        }]);
-      });
+    spyOn(localStorage,'getItem').and.callFake( () =>{
+      return JSON.stringify([{
+        productid:prodect1,
+        quantity: 2,
+        deliveryOptionsid:'1'
+      },
+      {
+        productid:prodect2,
+        quantity: 1,
+        deliveryOptionsid:'2'
+      }]);
+    });
 
-    loadingFormsStorage();
+   loadingFormsStorage();
 
-    renderPage();
+   renderPage();
   });
 
   it('display the cart', () =>{
@@ -55,10 +54,11 @@ beforeAll((done)=>{
 
     expect( document.querySelector(`.js-container-${prodect1}`) ).toEqual(null);
     expect( document.querySelector(`.js-container-${prodect2}`) ).not.toEqual(null);//-->This told not equal to null so that means some data or something present in the dom.
-    expect( localStorage.setItem).toHaveBeenCalledTimes(1)  
+    expect( localStorage.setItem).toHaveBeenCalledTimes(2); //--> This just check how many times the localStorage.setItem called after deleting one product from the cart.
   });
 
   afterEach( ()=>{ //--> This just run after the it() block run this just clean up the html that's all.
     document.querySelector('.js-test-container').innerHTML ='';
   });
+
 }); 
